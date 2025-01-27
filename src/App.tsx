6957bdf99cd0d4e7201cff9ae3158ef6e25fee9b
Willy Everton S. Nascimento <willy.nascimento@squadra.com.br>
2025-01-27 20:27:00 -0300

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import { TaskProvider } from './context/TaskContext';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TaskProvider>
+      <div style={{ padding: 20 }}>
+        <TaskForm />
+        <TaskList />
+      </div>
+    </TaskProvider>
   );
-}
+};
 
 export default App;
